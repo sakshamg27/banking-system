72fcb4e75d8dca14919ed5c22f8f98b29798267e
sakshamg27 <87021967+sakshamg27@users.noreply.github.com>
2021-09-02 09:33:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function sendMoney(){
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+   var enterSName = document.getElementById("enterSName").value;
+   var findSenderBankAccount = enterSName + "BankBalance";
+   var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
+   if (enterAmount > enterSAmount) {
+      alert("Insufficient Balance.")
+   }
+else {
+      var findUserBankAccount = enterName + "BankBalance";
+     
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById(findSenderBankAccount).innerHTML) - enterAmount;
+      document.getElementById(findSenderBankAccount).innerHTML = myAccountBalance;
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(`Your Transaction is Successful !  
+      ₹${enterAmount} is conveyed to ${enterName}@yahoo.com.`)
+
+      // transaction history 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`₹ ${enterAmount} is sent from the sender with Email-id ${enterSName}@eyahoo.com
+ to recepient with Email-id ${enterName}@yahoo.com on ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
